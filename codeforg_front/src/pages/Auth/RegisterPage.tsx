6c6af14a611d681eda7814b3db35c0876c9736ed
soufianenajim99 <soufianenajim100@gmail.com>
2025-03-11 07:56:46 +0000

    PFR-24/feature/working-on-auths

--- codeforg_front/src/pages/Auth/RegisterPage.tsx
+++ codeforg_front/src/pages/Auth/RegisterPage.tsx
@@ -6,7 +6,6 @@ const RegisterPage = () => {
     <div className="min-h-screen bg-gray-100">
       <Navbar />
 
-      {/* Sign Up Form */}
       <div className="w-full flex items-center justify-center py-16">
         <div className="bg-white w-96 p-8 rounded-lg shadow-lg">
           <h2 className="text-2xl font-bold text-center mb-6">
